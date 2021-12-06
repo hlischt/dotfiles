61f9100b9963b110fa4b6809bf2a175b7c9c5087
hlischt <50388907+hlischt@users.noreply.github.com>
2021-12-06 17:41:52 -0600

    Change '+' with '\{1,\}' for compatibility reasons

--- bin/first_ep.sh
+++ bin/first_ep.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Plays the first episode from a random series
 
-ep="$(find . -type f -regex '.*/[^/]*\([^0-9/S]0+1\|E0+1\)[^0-9/][^/]*\(mkv\|mp4\|avi\)' | shuf | head -n1)"
+regex='.*/[^/]*\([^0-9/S]0\{1,\}1\|E0\{1,\}1\)[^0-9/][^/]*\(mkv\|mp4\|avi\)'
+ep="$(find . -type f -regex "$regex" | shuf | head -n1)"
 mpv -- "$ep"
