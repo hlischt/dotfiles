b47522d8c66e165e8a1d60b11f8f27bb068b6120
hlischt <50388907+hlischt@users.noreply.github.com>
2021-12-29 00:08:39 -0600

    Comment in script

--- bin/first_ep.sh
+++ bin/first_ep.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Plays the first episode from a random series
+# Busybox's find worked well without the regextype flag
+# Guess it's a GNU quirk
 
 regex='.*/[^/]*\([^0-9/S]0\{1,\}1\|E0\{1,\}1\)[^0-9/][^/]*\(mkv\|mp4\|avi\)'
 ep="$(find "$1" -type f -regextype grep -regex "$regex" | shuf | head -n1)"
