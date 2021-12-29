dd82fefbe6c6108e85f4477b26d53281a4a2be29
hlischt <50388907+hlischt@users.noreply.github.com>
2021-12-29 00:06:33 -0600

    first_ep: add regextype for GNU find, I guess

--- bin/first_ep.sh
+++ bin/first_ep.sh
@@ -2,5 +2,5 @@
 # Plays the first episode from a random series
 
 regex='.*/[^/]*\([^0-9/S]0\{1,\}1\|E0\{1,\}1\)[^0-9/][^/]*\(mkv\|mp4\|avi\)'
-ep="$(find . -type f -regex "$regex" | shuf | head -n1)"
+ep="$(find "$1" -type f -regextype grep -regex "$regex" | shuf | head -n1)"
 mpv -- "$ep"
